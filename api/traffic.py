a30be9becb2db6019303819479978371e10d08c7
Artur Baruchi <baruchi@nic.br>
2017-12-25 21:30:30 -0200

    Adds a header to files robot.py and traffic.py

--- api/traffic.py
+++ api/traffic.py
@@ -1,2 +1,15 @@
 """ This script is related to the traffic API connection and queries
 """
+
+# System Imports
+
+
+# Third-party Imports
+
+
+# Local source tree Imports
+
+
+
+
+
--- robot.py
+++ robot.py
@@ -1,2 +1,17 @@
 """ This script is related to the telegram API connection and commands
 """
+
+# System Imports
+
+
+# Third-party Imports
+from telegram import
+
+# Local source tree Imports
+from utils import  read_telegram_config
+
+
+
+
+
+
