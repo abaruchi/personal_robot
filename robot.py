948ff3abec9dc5bcac56d84d3addb8ae3f56959c
Artur Baruchi <baruchi@nic.br>
2018-01-05 00:27:01 -0200

    Fixes a issue related to handle location filters

--- robot.py
+++ robot.py
@@ -7,58 +7,13 @@
 # Third-party Imports
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
                           ConversationHandler)
-from telegram import (KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove)
 
 
 # Local source tree Imports
 from utils import read_telegram_config
-from api.traffic import TrafficInformation
-from handlers import commands, messages
-
-MY_LOCATION, OTHER_LOCATION, DIRECTION = range(3)
+from handlers import commands, messages, conversations
 
 
-# Routines to handle Location
-def lets_go(bot, update):
-    """
-
-    :param bot:
-    :param update:
-    :return:
-    """
-    message = """
-Here we go again... Dude, I dont have a crystal ball!\n
-Send me your location, before I give up helping you...\n    
-    """
-
-    bot.sendMessage(
-        update.message.chat_id,
-        text=message,
-        reply_markup=ReplyKeyboardMarkup(
-            [[KeyboardButton("Send My Location", request_location=True)]],
-            one_time_keyboard=True
-        )
-    )
-    return MY_LOCATION
-
-
-def my_current_address(bot, update):
-    gmaps = TrafficInformation()
-    latitude = update.message.location.latitude
-    longitude = update.message.location.longitude
-
-    coord_tuples = (latitude, longitude)
-    address = gmaps.current_address(coord_tuples)
-    addr_01 = address['street'] + "," + address['number'] + "-" + \
-              address['neibor']
-    return ConversationHandler.END
-
-
-def cancel(bot, update):
-    update.message.reply_text('Shit.. I always waste my time with you.. ',
-                              reply_markup=ReplyKeyboardRemove())
-
-    return ConversationHandler.END
 
 ###############################
 
@@ -70,6 +25,26 @@ def main():
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('letsgo', conversations.lets_go)],
+        states={
+            conversations.MY_LOCATION:
+                [MessageHandler(Filters.location,
+                                conversations.my_current_address,
+                                pass_user_data=True)],
+            conversations.OTHER_LOCATION:
+                [MessageHandler(Filters.text,
+                               conversations.my_other_address,
+                               pass_user_data=True)],
+            conversations.DIRECTION:
+                [MessageHandler(Filters.text,
+                                conversations.my_directions,
+                                pass_user_data=True)]
+        },
+        fallbacks=[CommandHandler('done', conversations.cancel)]
+    )
+    dp.add_handler(conv_handler)
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", commands.start))
@@ -83,16 +58,6 @@ def main():
                                   pass_args=True))
     dp.add_handler(MessageHandler(Filters.location,
                                   messages.coordinates_to_address))
-
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('letsgo', lets_go)],
-        states={
-            MY_LOCATION: [MessageHandler(Filters.location,
-                                         my_current_address)]
-        },
-        fallbacks=[CommandHandler('done', cancel)]
-    )
-    dp.add_handler(conv_handler)
 
     # Start the Bot
     updater.start_polling()
