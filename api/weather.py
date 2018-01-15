1421f94a5e7103f62a6968396f8f2df4f8e24c3b
Artur Baruchi <baruchi@nic.br>
2018-01-15 21:17:39 -0200

    Implements class and methods to handle weather forecasting

--- api/weather.py
+++ api/weather.py
@@ -1,6 +1,10 @@
 """ This script is related to the weather API connection and queries
 """
 
+from datetime import datetime
+
+from forecastio import load_forecast
+
 from utils import Regex, read_weather_config
 
 
@@ -9,11 +13,80 @@ class WeatherInformation(object):
 
     """
 
-    def __init__(self):
+    def __init__(self, lat=None, long=None, defaultplace=None):
         weather_conf = read_weather_config("config.ini")
         self.weather_key = weather_conf["token"]
 
-        if "home_coordinates" in weather_conf.keys():
-            self.home_coordinates = weather_conf["home_coordinates"]
-        elif "work_coordinates" in weather_conf.keys():
-            self.work_coordinates = weather_conf["work_coordinates"]
+        if lat is None or long is None:
+            if defaultplace == "home":
+                if "home_coordinates" in weather_conf.keys():
+                    home_coordinates = weather_conf["home_coordinates"]
+                    lat, long = map(float, home_coordinates.split(','))
+                    self.latitute, self.longitute = lat, long
+
+            elif defaultplace == "work":
+                if "work_coordinates" in weather_conf.keys():
+                    work_coordinates = weather_conf["work_coordinates"]
+                    lat, long = map(float, work_coordinates.split(','))
+                    self.latitute, sel.longitute = lat, long
+        else:
+            self.latitute, self.longitute = lat, long
+        self.unit = 'si'
+
+    def __forecast_init(self):
+        """
+
+        :return:
+        """
+        forecast = load_forecast(self.weather_key,
+                                 self.latitute,
+                                 self.longitute,
+                                 units=self.unit)
+        return forecast
+
+    def get_current_rain_probability(self):
+        """
+
+        :return:
+
+        """
+        forecast = self._forecast_init()
+        forecast_current = forecast.currently()
+        return forecast_current.precipProbability
+
+    def get_current_summary(self):
+        """
+
+        :return:
+        """
+        forecast = self._forecast_init()
+        forecast_current = forecast.currently()
+        return forecast_current.summary
+
+    def get_hour_rain_probability(self):
+        """
+
+        :return:
+        """
+        prob_dict = dict()
+        forecast = self._forecast_init()
+        forecast_daily = forecast.hourly()
+
+        for h in forecast_daily.data[0:6]:
+            prob_dict[str(h.time)] = h.precipProbability
+
+        return prob_dict
+
+    def get_hour_summary(self):
+        """
+
+        :return:
+        """
+        summ_dict = dict()
+        forecast = self._forecast_init()
+        forecast_daily = forecast.hourly()
+
+        for h in forecast_daily.data[0:6]:
+            summ_dict[str(h.time)] = h.summary
+
+        return summ_dict
