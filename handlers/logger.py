5640a815dab5b42630a6911778485ed79081fc24
Artur Baruchi <baruchi@nic.br>
2018-04-13 15:17:31 -0300

    Implements log class to handle bot messages

--- handlers/logger.py
+++ handlers/logger.py
@@ -2,23 +2,38 @@
 """
 
 import logging
-import os
-
-from utils import ConfigRead
+import logging.handlers
 
 
 class Log(object):
     """
     """
-    def __init__(self):
-        read_config = ConfigRead("config.ini")
-        self.global_conf = read_config.read_global_config()
-        self.log_format = '%(asctime)s - %(name)s - %(processName)s ' \
-                          '- %(message)s'
 
-    def __create_log_file(self):
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        logger_formatter = logging.Formatter('%(asctime)s - %(name)s - %('
+                                             'processName)s - %(message)s')
+
+
+        file_to_log = "telegram_bot.log"
+        logHandler = logging.handlers.TimedRotatingFileHandler(file_to_log,
+                                                                when='W6',
+                                                                interval=1,
+                                                                backupCount=3)
+        logHandler.setLevel(logging.INFO)
+        logHandler.setFormatter(logger_formatter)
+        self.logger.addHandler(logHandler)
+
+    def log_my_robot (self, contact_name, command, log_message):
         """
 
+        :param contact_name:
+        :param command:
+        :param log_message:
         :return:
         """
-        pass
+        log = "Contact: {}, Command: {} - {}".format(contact_name,
+                                                     command,
+                                                     log_message)
+        self.logger.info(log)
